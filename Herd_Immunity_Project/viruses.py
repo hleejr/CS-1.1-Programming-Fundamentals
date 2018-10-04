42ecf29b65fc4b624134d8bd76cc20662c730b90
Drake Vorndran <DrakeVorndranOffical@gmail.com>
2018-10-04 13:57:46 -0700

    testing errors

--- Herd_Immunity_Project/viruses.py
+++ Herd_Immunity_Project/viruses.py
@@ -2,7 +2,7 @@ from virus import *
 
 viruses = [
 Virus("Anthrax", .325, .20),
-Virus("Bubonic Plague", .05, 1),
+Virus("Bubonic Plague", .5, 1),
 Virus("Chicken Pox", .435, 0),
 Virus("Ebola", .125, .7),
 Virus("Cold", .3, 0),
